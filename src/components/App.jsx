e7f09f1289b1e55ecb6d5ec3730ff1af60f476cf
RiskyClick <jonn.thinkrtc@gmail.com>
2017-08-12 03:17:47 +0000

    create profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,44 @@
+/*global fetch*/
 import React, { Component } from 'react';
-import { FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap '
+import { FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap'
 
 
 
 class App extends Component  {
+    constructor(props){
+        super(props);
+        
+        this.state = {
+          query: '',
+          artist: null
+            
+        };
+        
+    }
+    
+    
+    search(){
+        console.log('this.state', this.state);
+        const BASE_URL = 'https://api.spotify.com/v1/search?';
+        const FETCH_URL = BASE_URL + 'q=' + this.state.query
+                            + '&type=artist&limit=1';
+        console.log('FETCH_URL', FETCH_URL);
+        
+        fetch(FETCH_URL, {
+            method: 'GET'
+            
+        })
+        .then(response => response.json())
+        .then(json => {
+            const artist = json.artist.items[0];
+            console.log('artist', artist);
+            this.setState({artist});
+            
+        });
+            
+        
+        
+    }
     
     render(){
         return (
@@ -14,8 +49,21 @@ class App extends Component  {
                     <FormControl
                         type="text"
                         placeholder="Search for an artist.."
+                        value={this.state.query}
+                        onChange={event => {this.setState({query: event.target.value})}}
+                        onKeyPress={ event =>{
+                            if (event.key === 'Enter'){
+                                this.search();
+                                
+                            }
+                            
+                        }
+                        
+                            
+                        }
+                        
                     />
-                    <InputGroup.Addon>
+                    <InputGroup.Addon onClick={() => this.search()}>
                         <Glyphicon glyph="search"> </Glyphicon>
                     </InputGroup.Addon>
                 </InputGroup>
