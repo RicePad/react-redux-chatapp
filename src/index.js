66d4a8ae27fb299b850e66c9924003f07a763c73
RiskyClick <jonn.thinkrtc@gmail.com>
2017-09-25 01:27:38 +0000

    set up react and react-dom

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// For Redux integration
+import { Provider } from 'redux';
+import { connectStore } from 'redux';
+
+
+const userList = document.getElementById('user-list');
+
+ReactDOM.render(
+  <h1>Hello World!</h1>, document.getElementById('user-list')
+  );
+
+
+
