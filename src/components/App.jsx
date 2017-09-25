7296f8cc894fc5c0207fe375561d55ab9a6889b4
RiskyClick <jonn.thinkrtc@gmail.com>
2017-09-25 17:45:43 +0000

    add footer, link, toDo and toDoList component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import Footer from './Footer';
+import AddTodo from '../containers/AddTodo'
+import VisibleTodoList from '../containers/VisibleTodoList'
+
 
 
 class App extends Component {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,54 @@
-import { combineReducers } from 'redux';
+// import { combineReducers } from 'redux';
 
 
-export default combineReducers({
+// export default combineReducers({
     
-});
+// });
+
+import {
+  ADD_TODO,
+  TOGGLE_TODO,
+  SET_VISIBILITY_FILTER,
+  VisibilityFilters,
+  SHOW_ALL
+} from '../actions'
+
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false
+        }
+      ]
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+        if (index === action.index) {
+          return Object.assign({}, todo, {
+            completed: !todo.completed
+          })
+        }
+        return todo
+      })
+    default:
+      return state
+  }
+}
+
+function visibilityFilter(state = SHOW_ALL, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return action.filter
+    default:
+      return state
+  }
+}
+
+function todoApp(state = {}, action) {
+  return {
+    visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+    todos: todos(state.todos, action)
+  }
+}
