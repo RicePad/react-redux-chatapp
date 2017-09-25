c62e1cc2506310df03d580abb70fe6391a99d84b
RiskyClick <jonn.thinkrtc@gmail.com>
2017-09-25 01:26:07 +0000

    first commit and configuration after cloned

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import App from './components/App';
-
-ReactDom.render(
-        <App />, document.querySelector(".container")
-        // <div>SpotiMusic</div>, document.querySelector(".container")
-);
