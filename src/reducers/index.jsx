25ba42d439aa28ad4fb0740da4f6f333aa57d6e5
RiskyClick <jonn.thinkrtc@gmail.com>
2017-09-25 04:41:05 +0000

    first commit and configuration after cloning set up

--- src/reducers/index.jsx
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-import UserData from './UserData';
-import ActiveUser from './ActiveUser';
-
-export default combineReducers({
-   users: UserData, 
-   activeUser: ActiveUser
-    
-});
