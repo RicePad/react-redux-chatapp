c34493ff6e500ab88f9f286ce59f0ed28b68d496
RiskyClick <jonn.thinkrtc@gmail.com>
2017-09-25 10:24:53 +0000

    finish adding actions

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -1,6 +1,20 @@
 export const ADD_TODO = "ADD_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+export const SHOW_ALL = 'SHOW_ALL';
+export const SHOW_COMPLETED = 'SHOW_COMPLETED';
+export const SHOW_ACTIVE = 'SHOW_ACTIVE';
+export const SET_VISIBILITY_FILTER = "SET_VISIBILITY_FILTER";
+
+
 
 let nextTodoId = 0;
+
+export const visbilityFilters =  {
+    SHOW_ALL,
+    SHOW_COMPLETED,
+    SHOW_ACTIVE
+    
+};
 
 export const addToDo = (text) => {
    return {
@@ -11,4 +25,22 @@ export const addToDo = (text) => {
        
    };
     
+};
+
+export const toggleToDo = (index) => {
+    return {
+        type: TOGGLE_TODO,
+        index
+        
+    };
+    
+};
+
+
+export const setVisibilityFilter = (filter) => {
+    return {
+        type: SET_VISIBILITY_FILTER,
+        filter
+        
+    };
 };
