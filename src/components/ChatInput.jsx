0cb3347515ea31563af09626c37a4d6d3b0d0959
RiskyClick <jonn.thinkrtc@gmail.com>
2017-09-29 02:32:16 +0000

    add navbar and chat input components

---
+++ src/components/ChatInput.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class ChatInput extends Component {
+    
+    render(){
+        return(
+            <h1>Hello from Chatinput component</h1>
+            );
+        
+    }
+}
+
+export default ChatInput;
