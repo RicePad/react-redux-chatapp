4e1f7dc65e64d08d0917b877c47897773aa3fd12
RiskyClick <jonn.thinkrtc@gmail.com>
2017-09-29 02:43:54 +0000

    add ChatInput component to display message from user

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Appbar from './Appbar';
+import ChatInput from './ChatInput';
 
 class App extends Component {
     
     render(){
         return(
-            <h1>Hello from App component <Appbar /></h1>
+            <section>
+            <h1>Hello World from App Component</h1>
+            <Appbar />
+            <ChatInput />
+            </section>
             );
         
     }
--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
 
 class ChatInput extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            messageValue: ''
+        };
+    }
+    
+    handleChatMessage(event){
+        this.setState({ messageValue: event.target.value});
+    }
+    
+    handlePostClick(event){
+        console.log('button clicked!')
+    }
+    
+    
     
     render(){
         return(
-            <h1>Hello from Chatinput component</h1>
+           <div>
+            User 1
+            <br/>
+            <input
+                type="text"
+                className="messageValueInput"
+                id="messageValueInput"
+                name="messageValueInput"
+                key="messageValueInput"
+                value={this.state.messageValue}
+                onChange={(event) => {this.handleChatMessage(event)}}
+            />
+            <br />
+            <Button bsStyle="primary" onClick={(event) =>  {this.handlePostClick(event)}}> Send Message</Button>
+           </div>
+           
             );
         
     }
