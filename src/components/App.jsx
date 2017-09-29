af4d7ee19c34d07515811ad58232f6536dc223f5
RiskyClick <jonn.thinkrtc@gmail.com>
2017-09-29 03:17:22 +0000

    add bootstrap style to main App component and imported to bring all components together

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 import Appbar from './Appbar';
 import ChatInput from './ChatInput';
 import GroupChat from './GroupChat'
+import { Grid, Row, Col } from 'react-bootstrap'
 
 class App extends Component {
     
     render(){
         return(
-            <section>
-                <h1>Hello World from App Component</h1>
+            <div className="App">
                 <Appbar />
-                <ChatInput />
-                <GroupChat />
-            </section>
+                <Grid>
+                    <Row className="show-grid">
+                        <Col xs={12} md={4}>
+                          <ChatInput />
+                        </Col>
+                        <Col xs={12} md={8}>
+                            <GroupChat />
+                        </Col>
+                    </Row>
+                    
+                </Grid>
+            </div>
             );
         
     }
